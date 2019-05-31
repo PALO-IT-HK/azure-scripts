e2c6bc68cdfe717fd5692b1ee5fe527ece2b4834
Clement CHOI <cchchoi1986@gmail.com>
2019-05-31 10:22:31 +0800

    mod install agent file

--- pwsh-scripts/install-agent.ps1
+++ pwsh-scripts/install-agent.ps1
@@ -4,8 +4,8 @@ cd agent
 
 $version = "vsts-agent-win-x64-2.150.3.zip"
 
-Invoke-WebRequest -Uri "https://vstsagentpackage.azureedge.net/agent/2.150.3/"$version -OutFile "C:\agent\"$version
+Invoke-WebRequest -Uri "https://vstsagentpackage.azureedge.net/agent/2.150.3/$($version)" -OutFile "C:\agent\$($version)"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-[System.IO.Compression.ZipFile]::ExtractToDirectory("C:\agent\"$version, "$PWD")
+[System.IO.Compression.ZipFile]::ExtractToDirectory("C:\agent\$($version)", "$PWD")
